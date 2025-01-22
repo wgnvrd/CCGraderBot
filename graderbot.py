1cfef6309790fc2e52dd7be0eccb6726a53d1272
Isabelle Wagenvoord <isabellewagenvoord@gmail.com>
2025-01-22 13:41:20 -0700

    scaffolding for argparse demo

---
+++ graderbot.py
@@ -0,0 +1,13 @@
+import argparse
+# 1) import grade_assignment 
+
+# python graderbot.py grade course_id=203948 assignment_id=25832 user_id=2589023 score=2
+# 2) python graderbot.py grade --course_id 203948 --assignment_id 25832 --user_id 2589023 --score 2
+# argparse documentation: https://docs.python.org/3/library/argparse.html
+# argparse tutorial: https://docs.python.org/3/howto/argparse.html
+
+def grade():
+    pass
+
+def download_zip():
+    pass
