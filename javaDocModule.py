71ae49e84ae83a50f64a436e5b2c1c8ff08e092e
Kiernan Nesslar <nesmcq@gmail.com>
2025-02-05 10:56:05 -0700

    javadoc testing module based off of mcscn code

---
+++ javaDocModule.py
@@ -0,0 +1,22 @@
+from test_module import TestModule
+from check_file import check_file;
+class javaDocModule(TestModule):
+    files = []
+    def __init__(self,max, files):
+        self.__init__(max)
+        self.files = files
+
+
+    def run(self):
+        for f in self.files:
+            try:
+                with open(f) as file:
+                    problems = check_file(file)
+                if problems:
+                    self.score = 0
+                    self.feedback += f"\n{f}:"
+                    self.feedback += "\n".join(problems)
+
+            except FileNotFoundError:
+                print(f"\nFile \"{f}\" not found.")
+                exit(1)
