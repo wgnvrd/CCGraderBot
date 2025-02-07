42b9260cac4b94386236ddab74e00a0d1eb94493
KazuShimotake <chris.shimotake@gmail.com>
2025-02-07 00:35:44 -0700

    javadoc and pydoc aggregation works now, I knew it would be one fricking period throwing off all my filepaths

--- JavaDocAggregatorModule.py
+++ JavaDocAggregatorModule.py
@@ -1,35 +1,62 @@
 import os
 import subprocess
-
-import junitparser
-
 from pathlib import Path
-
-from glob import glob
-
-# remove this later
-import configparser
 from test_module import TestModule
-from CanvasHelper import get_submission
-from zipfile import ZipFile
+import shutil
 
 class JavaDocAggregatorModule(TestModule):
-    def __init__(self,name,source):
-        self.name = name
-        self.source = source
-
-        self.feedback = ""
-
+    """
+    This is one of the test modules that aggregates the javadoc
+    in all given java files and puts the output in a specific
+    directory called 'javadoc_aggregation_results'
+    """
+    def __init__(self,directory_name,filepaths):
+        """
+        This is the constructor for a JavaDocAggregatorModule
+        It takes in what you would like to call the folder where output will
+        be stored(directory_name)
+        and it also takes in a list of file paths to each of the files
+        for which documentation should be collected
+        """
+        self.name = directory_name
+        self.paths = filepaths
+        
     def run(self):
-        srcFiles = os.path.join(".","testing",self.source)
-        directory =  str(self.source) + "_" + str(self.name)
-        parent_dir = "."
-        path = os.path.join(parent_dir, directory)
-        os.makedirs(path)
-        print("Directory '%s' created" % directory)
-        subprocess.run(["javadoc","-d",path,srcFiles])
+        """
+        The primary method of the class
+        It loops through the list of given file paths and
+        aggregates documentation for each of them
+        """
+        for path in self.paths:
+            if not os.path.exists(path):
+                print("File at filepath " + path + " was not found")
+                return
+        print("All given filepaths lead to existing files")
+        
+        jdoc_agg_results_storage = Path("./javadoc_aggregation_results")
+        if not os.path.isdir(jdoc_agg_results_storage):
+            os.mkdir(jdoc_agg_results_storage)
+            print("Directory '%s' created" % jdoc_agg_results_storage)
+        
+        doc_agg = os.path.join(jdoc_agg_results_storage,self.name)
+        if os.path.isdir(doc_agg):
+            shutil.rmtree(doc_agg)
+        os.mkdir(doc_agg)
+        print("Directory '%s' created" % doc_agg)
 
-if __name__ == "__main__":    
-    srcFiles = Path("test_doc.java")
-    jdam = JavaDocAggregatorModule("John",srcFiles)
-    jdam.run()     
+        for path in self.paths:
+            source_name = path.rsplit("/",1)[1]
+            dirpath = os.path.join(doc_agg,source_name)
+            if os.path.isdir(dirpath):
+                shutil.rmtree(dirpath)
+            os.makedirs(dirpath)
+            print("Directory '%s' created" % dirpath)
+            subprocess.run(["javadoc","-d",dirpath,path])
+        
+
+if __name__ == "__main__": 
+    """
+    The pseudo main method of the file made just for testing
+    """
+    jdam = JavaDocAggregatorModule("John",["./testing/java_test_folder/java_test1.java","./testing/java_test_folder/java_test2.java"])
+    jdam.run()
--- PyDocAggregatorModule.py
+++ PyDocAggregatorModule.py
@@ -1,46 +1,57 @@
 import os
 import subprocess
-
-import junitparser
-
 from pathlib import Path
-
-from glob import glob
+from test_module import TestModule
 import shutil
 
-# remove this later
-import configparser
-from test_module import TestModule
-from CanvasHelper import get_submission
-from zipfile import ZipFile
-
 class PyDocAggregatorModule(TestModule):
-    def __init__(self,name,source):
-        self.name = name
-        self.source = source
-        self.feedback = ""
+    """
+    This is one of the test modules that aggregates the javadoc
+    in all given python files and puts the output in a specific
+    directory called 'pydoc_aggregation_results'
+    """
+    def __init__(self,directory_name,filepaths):
+        """
+        This is the constructor for a PyDocAggregatorModule
+        It takes in what you would like to call the folder where output will
+        be stored(directory_name)
+        and it also takes in a list of file paths to each of the files
+        for which documentation should be collected
+        """
+        self.name = directory_name
+        self.paths = filepaths
     
     def run(self):
-        srcFiles = os.path.join(".","testing",self.source)
-        directory =  str(self.source) + "_" + str(self.name)
-        parent_dir = "."
-        path = os.path.join(parent_dir, directory)
+        """
+        The primary method of the class
+        It loops through the list of given file paths and
+        aggregates documentation for each of them
+        """
+        for path in self.paths:
+            if not os.path.exists(path):
+                print("File at filepath " + path + " was not found")
+                return
+        print("All given filepaths lead to existing files")
         
-        oldfile = os.path.join(path,str(str(self.source)[:-3] + ".html"))
-        if os.path.exists(oldfile):
-            os.remove(oldfile)
-        if os.path.isdir(path):
-            os.rmdir(path)
+        pdoc_agg_results_storage = Path("./pydoc_aggregation_results")
+        if not os.path.isdir(pdoc_agg_results_storage):
+            os.mkdir(pdoc_agg_results_storage)
         
+        doc_agg = os.path.join(pdoc_agg_results_storage,self.name)
+        if os.path.isdir(doc_agg):
+            shutil.rmtree(doc_agg)
+        os.mkdir(doc_agg)
 
-        os.makedirs(path)
-        print("Directory '%s' created" % directory)
-        subprocess.run(["python","-m","pydoc","-w",srcFiles])
-        docfile = os.path.join(".",str(str(self.source)[:-3] + ".html"))
-        goal = os.path.join(".",directory)
-        shutil.move(docfile,goal)
+        for path in self.paths:
+            subprocess.run(["python","-m","pydoc","-w",path])
+            docfile = os.path.join(".",str(os.path.basename(path))[:-3] + ".html")  # os.path.join(".",str(str(path)[:-3] + ".html"))
+            shutil.move(docfile,doc_agg)
 
 if __name__ == "__main__":
-    srcFiles = Path("python_test.py")
-    pdam = PyDocAggregatorModule("Jane",srcFiles)
-    pdam.run()     
+    """
+    The pseudo main method of the file made just for testing
+    """
+    jdam = PyDocAggregatorModule("Jane",["./testing/python_test_folder/python_test1.py","./testing/python_test_folder/python_test2.py"])
+    # Yes, it does work on itself. Here's how to run it on the javadoc and pydoc aggregator modules
+    # jdam = PyDocAggregatorModule("Meta",["./PyDocAggregatorModule.py","./JavaDocAggregatorModule.py"])
+    jdam.run()    
